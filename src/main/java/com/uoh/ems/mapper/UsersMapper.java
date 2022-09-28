253e7ebfefe2d4cff569fe3fc06a770ceefe4d15
徐东升 <96970346+Xdong-sheng@users.noreply.github.com>
2022-09-28 08:18:23 +0800

    无法映射修改

--- src/main/java/com/uoh/ems/mapper/UsersMapper.java
+++
@@ -1,9 +0,0 @@
-package com.uoh.ems.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.uoh.ems.bean.Users;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UsersMapper extends BaseMapper<Users> {
-}
